c0af37ac6e600bc094dce2038955cc118c21af1d
yang <sc2818204580@163.com>
2020-05-16 18:03:18 +0800

    提交

---
+++ cases/login_test.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+# File    : login_test
+# Author  : yang
+# Email   : yang@163.com
+# Software: PyCharm
+# Time    : 2020/5/16 17:59
+import unittest
+class LoginTest(unittest.TestCase):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/login.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+# File    : login
+# Author  : yang
+# Email   : yang@163.com
+# Software: PyCharm
+# Time    : 2020/5/16 17:21
+from tools.service import Service
+
+
+class Login:
+
+    def __init__(self,driver):
+        self.driver=driver
+    #输入用户名
+    def input_uasername(self,uname):
+        username=self.driver.find_element_by_css_selector('div.row:nth-child(1) > input:nth-child(1)')
+        Service.send_input(username,uname)
+
+    #输入密码
+    def input_password(self,psword):
+        password=self.driver.find_element_by_css_selector('div.row:nth-child(2) > input:nth-child(1)')
+        Service.send_input(password,psword)
+
+    #输入验证码
+    def input_vcode(self,code):
+        vcode=self.driver.find_element_by_css_selector('input.col-md-6')
+        Service.send_input(vcode,code)
+    #取消勾选记住密码
+    def click_remember(self):
+        self.driver.find_element_by_css_selector('input.col-md-6').click()
+    #点击登录
+    def click_login(self):
+        self.driver.find_element_by_css_selector('.btn').click()
+    #点击登录
+    def do_login(self,uname,psword,code):
+        Service.open_startpage(self.driver)
+        self.input_uasername(uname)
+        self.input_password(psword)
+        self.input_vcode(code)
+        self.click_remember()
+        self.click_login()
+
+
+if __name__ == '__main__':
+    driver=Service.get_driver()
+    driver.implicitly_wait(10)
+    lo=Login(driver)
+    lo.do_login("WNCD000","woniu123","0000")
