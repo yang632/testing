4f09097c3d02566bc14b363d1c1dc9e1dcd9d446
hxy <1305149167@qq.com>
2020-05-17 16:33:42 +0800

    Update woniuboss4.0_case_huang.xlsx
    Signed-off-by: hxy <1305149167@qq.com>

---
+++ lib/teaching_management.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+#@Time      :2020/5/17
+#@Author    :hxy
+#@File      :teaching_management.py
+
+
+from tools.service import Service
+
+class TeachingManagement:
+    def __init__(self,driver):
+        self.driver = driver
+        #点击教学管理
+        self.driver.find_element_by_partial_link_text("教学管理").click()
+
+
+    # 点击课程安排
+    class CourseArrangement:
+        def __init__(self,driver):
+            self.driver = driver
+            self.driver.find_element_by_partial_link_text("课程安排").click()
+
+        # 点击查询
+        def click_query(self):
+            self.driver.find_element_by_css_selector("button.btn:nth-child(10)").click()
+
+        # 排课
+        def new_course(self):
+            # 点击排课按钮
+            self.driver.find_element_by_css_selector("button.btn:nth-child(11)").click()
