9b5440f17c87ff4aa9ab226806dad25e5eaf3de8
yang <sc2818204580@163.com>
2020-05-18 19:36:54 +0800

    提交

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,10 +5,3 @@
 # Software: PyCharm
 # Time    : 2020/5/16 15:50
 
-#
-# from selenium import webdriver
-# driver=webdriver.Edge()
-# alert=driver.switch_to.alert
-# driver.refresh()
-# driver.find_elements_by_xpath().te
-# driver.find_element_by_css_selector()
