0ccccaa7649260eb816870abe8f3c62bf63cd3bd
yang <sc2818204580@163.com>
2020-05-19 11:44:41 +0800

    提交代码

---
+++ lib/public_rescources.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# File    : public_rescources
+# Author  : yang
+# Email   : yang@163.com
+# Software: PyCharm
+# Time    : 2020/5/19 11:43
+
+class PublicRescources:
+    pass
