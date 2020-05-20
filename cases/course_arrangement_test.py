7d12d9f064ae9053ed610eb87a10be01574cb556
yang <sc2818204580@163.com>
2020-05-20 14:16:51 +0800

    Squashed commit of the following:
    commit 89afd035f6d711a10d76913c1838afd57376ea90
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 14:14:46 2020 +0800
    
        排课模块测试完成
    
    commit 812562e524ca743684cef1b30553eb37c69ed02a
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 14:03:57 2020 +0800
    
        提交
    
    commit 06907db24648148e28d52e2a437b229b5711b80a
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 11:50:49 2020 +0800
    
        完成课程安排模块
    
    commit f2591e2bd8650e4b4f90f28ea34050243aafcd1e
    Merge: 091380a bb3c360
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 11:49:14 2020 +0800
    
        Merge remote-tracking branch 'origin/master'
    
        # Conflicts:
        #	lib/course_arrangement.py
    
    commit 091380a3cabfbd9d7580b4480711b98035228339
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 10:55:47 2020 +0800
    
        课程安排模块完成
        Signed-off-by: hxy <1305149167@qq.com>
    
        Signed-off-by: hxy <1305149167@qq.com>
    
    commit bb3c36095d6bb2ef0147e5a0ff60dabd19499505
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 10:55:47 2020 +0800
    
        完成模块
    
    commit c3e9d3cf2495501a93e65e24239f17341eda15b0
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 10:02:12 2020 +0800
    
        增加修改班号
    
    commit 3f52b368cdf204695f418fab06cc0c4d9c25dff8
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 09:56:50 2020 +0800
    
        提交
    
    commit 9429e97772ff13c112c105f555edccded11c9b0a
    Merge: f688b38 c5340b1
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 09:54:19 2020 +0800
    
        Merge branch 'master' of github.com:yang632/testing
    
    commit f688b384ff0dd26b44a294334d0fe3ce3ddac4c1
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 09:53:00 2020 +0800
    
        提交
    
    commit f5efb1ee6b0268ab90145ce3bab76303a6bc62c4
    Author: hxy <1305149167@qq.com>
    Date:   Wed May 20 09:45:11 2020 +0800
    
        增加修改讲师
    
    commit 3c3f58bd312720cbfad3c71f15e8d7e7202902f8
    Author: hxy <1305149167@qq.com>
    Date:   Tue May 19 17:57:47 2020 +0800
    
        增加，排课、修改、结束时间
    
        Signed-off-by: hxy <1305149167@qq.com>

--- cases/course_arrangement_test.py
+++ cases/course_arrangement_test.py
@@ -18,7 +18,9 @@ class CouresArrangementTest(unittest.TestCase):
     # 获取添加的测试数据
     add_course_info = Utility.tran_tuple(contents[1])
     #获取查询的测试数据
-    query_coure_info = Utility.tran_tuple(contents[2])
+    query_course_info = Utility.tran_tuple(contents[2])
+    #获取修改的测试数据
+    alter_course_info = Utility.tran_tuple(contents[3])
 
     @classmethod
     def setUpClass(cls):
@@ -39,15 +41,14 @@ class CouresArrangementTest(unittest.TestCase):
 
     #测试增加
     @parameterized.expand(add_course_info)
-    def test_add_course(self,start_time,end_time,teacher,classroom,classcode,course,expect):
-        add_course_info = {"start_time":start_time,"end_time":end_time,"teacher":teacher,
+    def test_add_course(self,start_js,end_js,start_time,end_time,teacher,classroom,classcode,course,expect):
+        add_course_info = {"start_js":start_js,"end_js":end_js,"start_time":start_time,"end_time":end_time,"teacher":teacher,
                            "classroom":classroom,"classcode":classcode,"course":course}
 
         self.ca.do_add_course(add_course_info)
 
 #       执行测试后搜索
         self.ca.do_query(self.ca.query_all_course_info)
-#         // *[ @ id = "course_table"] / tbody / tr[1]
         teacher_list=Service.get_page_ele(self.driver,'//*[@id="course_table"]/tbody/tr[1]/td[1]')
         if teacher in teacher_list:
             actual='add-success'
@@ -56,7 +57,7 @@ class CouresArrangementTest(unittest.TestCase):
         self.assertEqual(actual,expect)
 
     # 测试查询
-    @parameterized.expand(query_coure_info)
+    @parameterized.expand(query_course_info)
     def test_query_coure(self,campus,teacher,speialty,start_tiame,end_tiame,expect):
         query_all_course_info = {'campus':campus, 'teacher':teacher, 'specialty': speialty,
                                  'start_time': start_tiame, 'end_time': end_tiame
@@ -71,6 +72,23 @@ class CouresArrangementTest(unittest.TestCase):
 
         self.assertEqual(actual,expect)
 
+    # 测试修改
+    @parameterized.expand(alter_course_info)
+    def test_alter_course(self,start_js,end_js,start_time,end_time,teacher,classroom,classcode,course,expect):
+        alter_course_info = {"start_js":start_js,"end_js":end_js,"start_time":start_time,"end_time":end_time,"teacher":teacher,
+                           "classroom":classroom,"classcode":classcode,"course":course}
+        self.ca.do_alter_course(alter_course_info)
 
+        classnum = self.ca.click_alter()
+
+        teacher_list=Service.get_page_ele(self.driver,'//*[@id="course_table"]/tbody/tr[1]/td[1]')
+
+        if teacher in teacher_list:
+            actual='add-success'
+        else:
+            actual='add-fail'
+        self.assertEqual(actual,expect)
+       
+       
 if __name__ == '__main__':
     pass
--- tools/service.py
+++ tools/service.py
@@ -108,21 +108,6 @@ class Service:
         js_value = f'{jspath}.value="{time}"'  # js添加时间
         driver.execute_script(js_value)
 
-# document.querySelector("#modifyCourseForm > div > div > div:nth-child(1) > input")
-
-  # 输入新增排课开始时间
-    def input_start_time(self, starttime):
-        js = 'document.querySelector("#addcourse > div.row > div:nth-child(1) > input").removeAttribute("readonly");'  # js去掉readonly属性
-        self.driver.execute_script(js)
-        js_value = f'document.querySelector("#addcourse > div.row > div:nth-child(1) > input").value="{starttime}"'  # js添加时间
-        self.driver.execute_script(js_value)
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
