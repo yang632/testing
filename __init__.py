88c2aa2631d9716c2d441eff10aa1d91b989f190
yang <sc2818204580@163.com>
2020-05-21 17:47:22 +0800

    提交

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-# -*- encoding: utf-8 -*-
-# File    : __init__.py
-# Author  : yang
-# Email   : yang@163.com
-# Software: PyCharm
-# Time    : 2020/5/16 15:51
 
 
-{"cus.customer_id":"731618",
- "cus.name":"亚索54","cus.sex":"女","cus.last_status":"02","cus.tel":"19876565568","cus.wechat":"0","cus.qq":"0","cus.school":"无","cus.education":"04","cus.major":"无","cus.intent":"无","cus.workage":"08","cus.salary":"0","cus.age":"","cus.source":"12","cus.email":"0","cus.eduexp":"无","cus.experience":"无","cus.last_tracking_remark":"已进行联系,报名倾向很高"}
+
+
+
+
+
+
+
+
+
