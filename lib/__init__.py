99f54f3d2551ff7694551642c75a7582ed3a5774
hxy <1305149167@qq.com>
2020-05-21 10:46:41 +0800

    提交

--- lib/__init__.py
+++ lib/__init__.py
@@ -9,24 +9,5 @@
 # from selenium.webdriver.common.by import By
 #
 # driver=webdriver.Edge()
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-# <<<<<<< HEAD
-# # driver=webdriver.Edge()
-# =======
-# >>>>>>> f3f751a66f81df62ed4ecb1fdd86f9cc888c31f2
-=======
 
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> eb59cd83fdf293eb2f507317e5606eea0a1c7eda
->>>>>>> 7893e9d29732b8fde83e05626c2ddc3ce7a21c50
-=======
->>>>>>> c305f2726377b474ddafb9ae8e4e9caf0f2198d5
-=======
->>>>>>> c305f2726377b474ddafb9ae8e4e9caf0f2198d5
